fe997026728e0da59b40f53d3851e840c40a7de2
Legend0619 <139355335+Legend0619@users.noreply.github.com>
2023-08-30 04:56:48 +0900

    layout

--- layouts/layout.js
+++ layouts/layout.js
@@ -4,7 +4,7 @@ import Navigation from "../components/global/Navigation";
 import Footer from "../components/global/Footer";
 function Layout(props) {
   return (
-    <Box maxWidth={"1920px"} m="0 auto" bg={props.bg}>
+    <Box maxWidth={"1930px"} m="0 auto" bg={props.bg}>
       <Navigation fixed={props.fixed} />
       {props.children}
       <Footer disclaimer />
